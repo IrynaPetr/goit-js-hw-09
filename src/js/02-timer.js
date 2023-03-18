7daf16073c62f9066dcba7d6e671aef2b5193b4e
IrynaPetr <iryna.bieliakova@gmail.com>
2023-03-19 01:37:37 +0200

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,89 +1,84 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css"
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const input = document.querySelector('#datetime-picker')
+
 const btnStart = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
 
-// btnStart.disabled = true;
-
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//     // if(selectedDates < Date.now) {
-//     //   return window.alert("Please choose a date in the future")
-//     // } if(selectedDates > Date.now) {
-//     //        btnStart.disabled = false;
-//     // };
-  
-// }
-//   };
-
-  const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-
-flatpickr(input, options);
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000))
+btnStart.disabled = true;
 
 
 
 
 
-const timer = {
+let userSelectDate = null;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates);
+    if(selectedDates[0] < new Date()) {
+      Notify.failure("Please choose a date in the future");
+      btnStart.disabled = true;
+     
+    } else {
+     btnStart.disabled = false;
+     userSelectDate = selectedDates[0];}
+  }}
+    const timer = {
   intervalId: null,
   isActive: false,
   start() {
     if(this.isActive) {
       return;
     }
-    const startTime = options.selectedDates;
+    
     this.isActive = true;
 
     this.intervalId = setInterval(() => {
       const currentTime = Date.now();
-      const deltaTime = startTime - currentTime;
+      const deltaTime = userSelectDate - currentTime;
       const timeComponents = convertMs(deltaTime);
-
+      updateClockFace(timeComponents);
      console.log(timeComponents);
-    }, 1000); 
-      console.log(startTime);
-      console.log(currentTime);
-      console.log(deltaTime);
-  }
+    }, 1000); }
+  
+}
+
+ 
+  flatpickr('#datetime-picker', {...options});
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateClockFace({ days, hours, minutes, seconds }) {
+  daysEl.textContent = `${days}`;
+  hoursEl.textContent = `${hours}`;
+  minutesEl.textContent = `${minutes}`;
+  secondsEl.textContent = `${seconds}`;
 }
 btnStart.addEventListener('click', () => {
   timer.start();
 });
-// timer.start()
+
